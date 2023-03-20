96bcf2652d3fcd6c1cfd94a6cce0386a3d5ea96b
VictorDalosto <victordalosto@gmail.com>
2023-03-20 14:04:43 -0300

    refactoring

--- ajusta-videoteca.js
+++ ajusta-videoteca.js
@@ -1,8 +1,10 @@
 /**
  * Script que adiciona funcionalidades para a pagina servicos.dnit.gov.br/videoteca
  * Funcionamento: Basta colocar o conteudo desse Javascript no console da pagina.
- * Versao: 1.0.0 - 20/03/2023
+ * Versao: 1.0.1 - 20/03/2023
  */
+
+const botoes = ["Corte", "Aterro", "Mista"];
 
 const marcacoesLocalStorage = JSON.parse(localStorage.getItem("marcacoes")) || [];
 
@@ -28,23 +30,21 @@ const marcacoesLocalStorage = JSON.parse(localStorage.getItem("marcacoes")) || [
     videoBody.append(latLong);
 })();
 
-const botao = document.createElement("button");
 (function criaBotaoMarcacoes() {
     const videoBody = document.querySelector("#mep_0").parentNode;
-    botao.innerHTML = "Marcar";
-    botao.setAttribute("id", "botaoMarcar");
-    botao.classList.add("btn", "btn-primary");
-    videoBody.append(botao);
-})();
-
-(function adicionaFuncionalidadeBotaoMarcar() {
-    botao.style.transition = "all 0.3s";
-    botao.addEventListener("click", () => {
+    botoes.forEach(b => {
+        const botao =  document.createElement("button");
+        botao.innerHTML = b;
+        botao.classList.add("btn", "btn-primary");
+        botao.style.marginRight = "15px";
+        videoBody.append(botao);
+        botao.style.transition = "all 0.3s";
+        botao.addEventListener("click", () => {
         const [lat, long] = document.querySelector("#coordenada-atual")
                                     .innerHTML.replace(/^Lat\/Long:\s*/, '')
                                     .split(", ").map(parseFloat);
         const odometro = parseFloat(document.querySelector("#HodometroValue").innerHTML);
-        const objeto = {"odom": odometro, "lat": lat, "long": long};
+        const objeto = {"marcacao":b, "odom": odometro, "lat": lat, "long": long};
         marcacoesLocalStorage.push(objeto);
         localStorage.setItem("marcacoes", JSON.stringify(marcacoesLocalStorage));
         botao.classList.add("btn-info");
@@ -52,32 +52,37 @@ const botao = document.createElement("button");
             botao.classList.remove("btn-info"); 
         }, 400)
     });
+    })
 })();
 
-const botaoLimpar = document.createElement("button");
-(function criaBotaoSalvarEmJSON() {
+(function criaBotaoLimpar() {
+    const botaoLimpar = document.createElement("button");
     const videoBody = document.querySelector("#mep_0").parentNode;
     botaoLimpar.innerHTML = "LIMPAR";
-    botaoLimpar.setAttribute("id", "botaoMarcar");
     botaoLimpar.classList.add("btn", "btn-secondary");
-    botaoLimpar.style.transition = "all 0.3s";
     botaoLimpar.style.marginBottom="15px"
     videoBody.prepend(botaoLimpar);
+    botaoLimpar.addEventListener("click", () => {
+        botaoLimpar.classList.add("btn-danger");
+        setTimeout(() => {
+            botaoLimpar.classList.remove("btn-danger"); 
+        }, 1500)
+        if (confirm('Voce quer limpar as marcacoes?')) {
+            marcacoesLocalStorage.length = 0;
+            localStorage.setItem("marcacoes", JSON.stringify(marcacoesLocalStorage));
+        }
+    });
 })();
 
-const botaoSalvar = document.createElement("button");
-(function criaBotaoSalvarEmJSON() {
+(function adicionaFuncionalidadeBotaoSalvar() {
+    const botaoSalvar = document.createElement("button");
     const videoBody = document.querySelector("#mep_0").parentNode;
     botaoSalvar.innerHTML = "SALVAR";
-    botaoSalvar.setAttribute("id", "botaoMarcar");
     botaoSalvar.classList.add("btn", "btn-primary");
     botaoSalvar.style.transition = "all 0.3s";
     botaoSalvar.style.marginBottom="15px"
     botaoSalvar.style.marginRight="15px"
     videoBody.prepend(botaoSalvar);
-})();
-
-(function adicionaFuncionalidadeBotaoSalvar() {
     botaoSalvar.addEventListener("click", () => {
         const jsonStr = JSON.stringify(marcacoesLocalStorage);
         const blob = new Blob([jsonStr], { type: "application/json" });
@@ -90,18 +95,5 @@ const botaoSalvar = document.createElement("button");
         setTimeout(() => {
             botaoSalvar.classList.remove("btn-info"); 
         }, 400)
-    });
-})();
-
-(function adicionaFuncionalidadeBotaoLimpar() {
-    botaoLimpar.addEventListener("click", () => {
-        if (confirm('Voce quer limpar as marcacoes?')) {
-            marcacoesLocalStorage.length = 0;
-            localStorage.setItem("marcacoes", JSON.stringify(marcacoesLocalStorage));
-            botaoLimpar.classList.add("btn-danger");
-            setTimeout(() => {
-                botaoLimpar.classList.remove("btn-danger"); 
-            }, 400)
-        }
     });
 })();
